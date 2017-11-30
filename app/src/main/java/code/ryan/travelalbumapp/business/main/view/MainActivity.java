284cce088f1bf977522354754fe3b9813a0e7616
lijianchang <liji.anchang@163.com>
2017-11-30 11:42:56 +0800

    完成NavigationMenu的UI设计

--- app/src/main/java/code/ryan/travelalbumapp/business/main/view/MainActivity.java
+++ app/src/main/java/code/ryan/travelalbumapp/business/main/view/MainActivity.java
@@ -1,5 +1,6 @@
 package code.ryan.travelalbumapp.business.main.view;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -18,8 +19,8 @@ import code.ryan.utils.DimensionUtil;
 
 public class MainActivity extends BaseActivity<IMainView, IMainPresenter> implements IMainView, NavigationView.OnNavigationItemSelectedListener {
     private Toolbar mToolbar;
-    private DrawerLayout mDrawer;
-    private NavigationView mNavigationView;     // View of DrawerLayout's left.
+    private DrawerLayout mDrawer;               // 抽屉布局
+    private NavigationView mNavigationView;     // 抽屉布局打开的菜单栏
 
     @Override
     public IMainPresenter setPresenter() {
@@ -36,14 +37,7 @@ public class MainActivity extends BaseActivity<IMainView, IMainPresenter> implem
         findView();
         initToolbar();
         initDrawer();
-    }
-
-    private void initDrawer() {
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawer, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        mDrawer.addDrawerListener(toggle);
-        toggle.syncState();
-        // Monitor the NavigationView open or close.
-        mNavigationView.setNavigationItemSelectedListener(this);
+        initNavigationMenu();
     }
 
     private void findView() {
@@ -55,21 +49,38 @@ public class MainActivity extends BaseActivity<IMainView, IMainPresenter> implem
     private void initToolbar() {
         setSupportActionBar(mToolbar);
 
-        // Makes the status bar be able to show the background photo.
+        // 让背景图片穿透Toolbar和状态栏
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        // Set the toolbar topMargin is statusBar's height.
+        // 设置Toolbar的marginTop属性值为状态栏的高度
         LinearLayout.LayoutParams param = (LinearLayout.LayoutParams) mToolbar.getLayoutParams();
         param.setMargins(0, DimensionUtil.getStatusBarHeight(getApplicationContext()), 0, 0);
         mToolbar.setLayoutParams(param);
     }
 
+    private void initDrawer() {
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawer, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawer.addDrawerListener(toggle);
+        toggle.syncState();
+        // 监听NavigationMenu的点击
+        mNavigationView.setNavigationItemSelectedListener(this);
+    }
+
+
+    private void initNavigationMenu() {
+        ColorStateList csl = getBaseContext().getResources().getColorStateList(R.color.navigation_menu_item_color);
+
+        // 设置NavigationBar文字的颜色
+        mNavigationView.setItemTextColor(csl);
+        // 设置NavigationBar按钮的颜色
+        mNavigationView.setItemIconTintList(csl);
+
+    }
 
     @Override
     public void onBackPressed() {
-        // Handle back key pressed
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+        // 处理返回按键事件，关闭抽屉
+        if (mDrawer.isDrawerOpen(GravityCompat.START)) {
+            mDrawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
@@ -77,25 +88,18 @@ public class MainActivity extends BaseActivity<IMainView, IMainPresenter> implem
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+        // 处理Navation菜单栏的点击
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_my_paper) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_popular_paper) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_advanced) {
 
         }
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        mDrawer.closeDrawer(GravityCompat.START);
         return true;
     }
 }
